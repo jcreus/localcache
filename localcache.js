1ff8b8cca14b7d18b27f4f7b42be23da99aca226
Joan Creus <joan.creus.c@gmail.com>
2012-02-25 16:31:40 +0100

    Major rewrite. Simpler and faster

--- localcache.js
+++ localcache.js
@@ -11,7 +11,6 @@ localcache = {};
 localcache.hasLocal = (('localStorage' in window) && typeof localStorage.getItem === 'function') ? true : false;
 
 localcache._image_loaded = function () {
-  var error = false;
   localcache.canvas.width = this.width;
   localcache.canvas.height = this.height;
   localcache.ctx.drawImage(this, 0, 0);
@@ -19,25 +18,16 @@ localcache._image_loaded = function () {
     var dataURL = localcache.canvas.toDataURL("image/png");
   } catch (exc) {
     console.error("Error! Maybe you were trying to load an image from another origin. Please do not try to cache such images.");
-    error = true;
-  }
-  var t = document.getElementsByClassName("cache");
-  for (var i=0; i<t.length; i++) {
-     var e = t[i];
-     if ((!error) && (e.getAttribute("data-src") == this.originalsrc)) {
-       e.setAttribute("src",dataURL);
-     } else {
-       e.setAttribute("src",e.getAttribute("data-src"));
-     }
+    return
   }
   try {
-    localStorage.setItem("cached-"+this.originalsrc,JSON.stringify({data:dataURL,date:new Date().toString()}));
+    localStorage.setItem("cached-"+this.getAttribute("data-src"),JSON.stringify({data:dataURL,date:new Date().toString()}));
   } catch(e) {
     console.error("localStorage error! (Maybe, quota)");
   }
 };
 
-localcache.load = function (images, params) {
+localcache.load = function (params) {
   var params = params || {};
   params.load = params.load || false;
   params.minDate = params.minDate || false;
@@ -46,34 +36,23 @@ localcache.load = function (images, params) {
     localcache.ctx = localcache.canvas.getContext("2d");
   }
   if (!params.load && localcache.hasLocal) {
-    for (var i=0; i<images.length; i++) {
-      var url = images[i];
-      var cached = localStorage.getItem("cached-"+url);
-      if (cached != null) {
-        var parsed = JSON.parse(cached);
-        var cached_date = new Date(parsed.date);
-        if (!params.minDate || (cached_date > params.minDate)) {
-          var t = document.getElementsByClassName("cache");
-          for (var y=0; y<t.length; y++) {
-            var e = t[y];
-            if (e.getAttribute("data-src") == url) {
-              e.setAttribute("src",parsed.data);
-            }
+    var all = document.getElementsByClassName("cache");
+    for (var i=0; i<all.length; i++) {
+      var img = all[i];
+      if (!params.load && localcache.hasLocal) {
+        var cached = localStorage.getItem("cached-"+img.getAttribute("data-src"));
+        if (cached != null) {
+          var parsed = JSON.parse(cached);
+          var cached_date = new Date(parsed.date);
+          if (!params.minDate || (cached_date > params.minDate)) {
+            img.setAttribute("src",parsed.data);
           }
           continue;
         }
       }
-      var image = new Image();
-      image.src = url;
-      image.originalsrc = url;
-      image.onload = localcache._image_loaded.bind(image);
-      image.onerror = function () { console.error("Image could not be loaded"); };
-    }
-  } else {
-    var t = document.getElementsByClassName("cache");
-    for (var i=0; i<t.length; i++) {
-       var e = t[i];
-       e.setAttribute("src",e.getAttribute("data-src"));
+      img.setAttribute("src",img.getAttribute("data-src"));
+      img.onload = localcache._image_loaded.bind(img);
+      img.onerror = function () { console.error("Image could not be loaded"); };
     }
   }
 }
